6c0b5542a7aba7080e3bb7600ebf9bdf5abfc1bb
ptrafecanty <ptrafecanty@gmail.com>
2025-11-05 14:44:31 -0700

    phase 1

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import {
+  CommandsRegistry,
+  registerCommand,
+  runCommand,
+} from "./commands/commands";
+import { handlerLogin, handlerRegister } from "./commands/users";
+
+async function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length < 1) {
+    console.log("usage: cli <command> [args...]");
+    process.exit(1);
+  }
+
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  const commandsRegistry: CommandsRegistry = {};
+
+  registerCommand(commandsRegistry, "login", handlerLogin);
+  registerCommand(commandsRegistry, "register", handlerRegister);
+
+  try {
+    await runCommand(commandsRegistry, cmdName, ...cmdArgs);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error running command ${cmdName}: ${err.message}`);
+    } else {
+      console.error(`Error running command ${cmdName}: ${err}`);
+    }
+    process.exit(1);
+  }
+  process.exit(0);
+}
+
+main();
